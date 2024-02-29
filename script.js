8ab43c30895fd56676d66408fb32d2467da237f2
unknown <https://github.com/vinins1/projeto01.git>
2024-02-29 14:36:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Dando funcionalidade ao botao Dark/Light
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
+
